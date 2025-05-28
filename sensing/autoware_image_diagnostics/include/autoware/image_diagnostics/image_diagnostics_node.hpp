d3fb7ec884b0f79f3bc56fb61502dc3884ef12d3
vividf <yihsiang.fang@tier4.jp>
2025-05-28 22:14:02 +0900

    chore: clean code
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- sensing/autoware_image_diagnostics/include/autoware/image_diagnostics/image_diagnostics_node.hpp
+++ sensing/autoware_image_diagnostics/include/autoware/image_diagnostics/image_diagnostics_node.hpp
@@ -47,7 +47,7 @@ namespace autoware::image_diagnostics
 class ImageDiagNode : public rclcpp::Node
 {
 private:
-  enum Image_State : uint8_t {
+  enum ImageState : uint8_t {
     NORMAL,
     SHADOW_CLIPPING,
     BLOCKAGE,
@@ -59,12 +59,12 @@ private:
   {
     std::string label;
     std::string key_prefix;
-    Image_State state_enum;
+    ImageState state_enum;
     int threshold;
     int count = 0;
     float ratio = 0.0f;
 
-    StateInfo(std::string label, std::string key_prefix, Image_State state_enum, int threshold)
+    StateInfo(std::string label, std::string key_prefix, ImageState state_enum, int threshold)
     : label(std::move(label)),
       key_prefix(std::move(key_prefix)),
       state_enum(state_enum),
@@ -72,7 +72,7 @@ private:
     {
     }
 
-    void update_count_and_ratio(const std::vector<Image_State> & states, int total_blocks)
+    void update_count_and_ratio(const std::vector<ImageState> & states, int total_blocks)
     {
       count = static_cast<int>(std::count(states.begin(), states.end(), state_enum));
       ratio = static_cast<float>(count) / static_cast<float>(total_blocks);
@@ -96,20 +96,14 @@ private:
     }
   };
 
-  std::vector<StateInfo> state_infos_;
-
-  std::unordered_map<Image_State, cv::Scalar> state_color_map_ = {
-    {Image_State::NORMAL, cv::Scalar(100, 100, 100)},
-    {Image_State::SHADOW_CLIPPING, cv::Scalar(0, 0, 0)},
-    {Image_State::BLOCKAGE, cv::Scalar(0, 0, 200)},
-    {Image_State::LOW_VISIBILITY, cv::Scalar(0, 200, 200)},
-    {Image_State::HIGHLIGHT_CLIPPING, cv::Scalar(200, 0, 200)},
+  struct RegionFeatures
+  {
+    std::vector<int> avg_intensity;
+    std::vector<float> blockage_ratio;
+    std::vector<float> frequency_mean;
+    cv::Mat frequency_map;
   };
 
-  cv::Scalar border_color_ = cv::Scalar(255, 255, 255);
-
-  int total_blocks_{0};
-  std::deque<bool> recent_error_flags_;
   struct Parameters
   {
     // General settings
@@ -138,26 +132,32 @@ private:
     double low_visibility_frequency_threshold;
   } params_;
 
-  struct RegionFeatures
-  {
-    std::vector<int> avg_intensity;
-    std::vector<float> blockage_ratio;
-    std::vector<float> frequency_mean;
-    cv::Mat frequency_map;
+  std::unordered_map<ImageState, cv::Scalar> state_color_map_ = {
+    {ImageState::NORMAL, cv::Scalar(100, 100, 100)},
+    {ImageState::SHADOW_CLIPPING, cv::Scalar(0, 0, 0)},
+    {ImageState::BLOCKAGE, cv::Scalar(0, 0, 200)},
+    {ImageState::LOW_VISIBILITY, cv::Scalar(0, 200, 200)},
+    {ImageState::HIGHLIGHT_CLIPPING, cv::Scalar(200, 0, 200)},
   };
+
+  cv::Scalar border_color_ = cv::Scalar(255, 255, 255);
+
+  int total_blocks_{0};
+  std::deque<bool> recent_error_flags_;
+  std::vector<StateInfo> state_infos_;
 
   void check_parameters() const;
   void run_image_diagnostics(const sensor_msgs::msg::Image::ConstSharedPtr input_image_msg);
   cv::Mat preprocess_image(const sensor_msgs::msg::Image::ConstSharedPtr & msg) const;
   RegionFeatures compute_image_features(const cv::Mat & gray_image) const;
-  std::vector<Image_State> classify_regions(const RegionFeatures & features) const;
-  cv::Mat generate_diagnostic_image(const std::vector<Image_State> & states, const cv::Size & size);
+  std::vector<ImageState> classify_regions(const RegionFeatures & features) const;
+  cv::Mat generate_diagnostic_image(const std::vector<ImageState> & states, const cv::Size & size);
   void publish_debug_images(
     const std_msgs::msg::Header & header, const cv::Mat & gray_image, const cv::Mat & dft_image,
     const cv::Mat & diagnostic_image);
   static void shift_image(cv::Mat & img);
   void update_image_diagnostics(
-    const std::vector<Image_State> & states, const rclcpp::Time & timestamp);
+    const std::vector<ImageState> & states, const rclcpp::Time & timestamp);
 
   std::unique_ptr<autoware_utils_diagnostics::DiagnosticsInterface> diagnostics_interface_;
 
