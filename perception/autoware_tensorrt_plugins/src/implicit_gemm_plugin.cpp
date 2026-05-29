811ed125491d8f48ad6f99917c1038825d2aaf55
vividf <yihsiang.fang@tier4.jp>
2026-05-29 16:21:47 +0900

    chore: fix cspell
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- perception/autoware_tensorrt_plugins/src/implicit_gemm_plugin.cpp
+++ perception/autoware_tensorrt_plugins/src/implicit_gemm_plugin.cpp
@@ -371,7 +371,7 @@ std::int32_t ImplicitGemmPlugin::enqueue(
 
   [[maybe_unused]] auto const conv_run_status = ConvGemmOps::implicit_gemm(
     alloc2, *tuner_ptr, input_features, weights, pair_fwd, pair_mask_splits, mask_argsort_splits,
-    num_act_out, mask_tensor_, arch_, false, params_.is_subm,
+    num_act_out, mask_tensor_, arch_, false, params_.is_subm,  // cSpell:ignore subm
     reinterpret_cast<std::uintptr_t>(stream), tv::CUDAKernelTimer(false), true, false, bias_tv,
     params_.act_alpha, params_.act_beta, activation_from_int(params_.act_type), false, 1.0,
     tv::Tensor(), tv::Tensor(), 0.0, -1);
